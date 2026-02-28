df94f582ede575e90853420b861eb5badf778ef6
muneebkattody <m.kattody@proton.me>
2026-02-28 13:36:58 +0530

    Cors change

--- src/main/java/com/eatyaar/config/CorsConfig.java
+++ src/main/java/com/eatyaar/config/CorsConfig.java
@@ -13,9 +13,11 @@ public class CorsConfig {
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.addAllowedOrigin("http://localhost:5173"); // Vite dev server
-        config.addAllowedMethod("*");  // GET, POST, PUT, PATCH, DELETE
-        config.addAllowedHeader("*");  // Authorization, Content-Type etc
+        config.addAllowedOrigin("http://localhost:5173");
+        config.addAllowedOrigin("https://eatyaar.in");
+        config.addAllowedOrigin("https://www.eatyaar.in");
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
         config.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
