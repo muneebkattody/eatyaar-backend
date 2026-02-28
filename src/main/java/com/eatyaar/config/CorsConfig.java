d91392e4fe5126fc59c71a54342a801e79a37b46
muneebkattody <m.kattody@proton.me>
2026-02-28 13:46:24 +0530

    fix: cord error - add domains

--- src/main/java/com/eatyaar/config/CorsConfig.java
+++ src/main/java/com/eatyaar/config/CorsConfig.java
@@ -3,6 +3,7 @@ package com.eatyaar.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
@@ -24,5 +25,20 @@ public class CorsConfig {
         source.registerCorsConfiguration("/api/**", config);
 
         return new CorsFilter(source);
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("http://localhost:5173");
+        config.addAllowedOrigin("https://eatyaar.in");
+        config.addAllowedOrigin("https://www.eatyaar.in");
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/api/**", config);
+        return source;
     }
 }
--- src/main/java/com/eatyaar/config/SecurityConfig.java
+++ src/main/java/com/eatyaar/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.eatyaar.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -24,6 +25,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(Customizer.withDefaults())
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session ->
